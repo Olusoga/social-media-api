9edb8c68d4e522c10067b1f741042ec9c9d4ce01
olusoga olalekan <olusogaolamileksy@gmail.com>
2024-07-05 20:06:05 +0100

    Implement getFeedPosts Function to Retrieve Posts by User's Following
    - Implemented getFeedPosts in postService.ts to fetch posts based on the user's following list.
    - Converted user.following from ObjectId[] to string[] and then to ObjectId for querying posts.
    - Added error handling to manage cases where the user is not found or posts retrieval fails.

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,8 +1,8 @@
-import { createPost, findPostsByUserIds, findPostById, updatePost } from '../repositories/post.repository';
+import { createPost, findPostById, updatePost, findPostsByAuthorIds } from '../repositories/post.repository';
 import { findUserById } from '../repositories/user.repository';
 import { IPost } from '../models/post.model';
 import { HttpError } from '../utils/error-handler';
-import { Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 const { ObjectId } = Types;
 
 export const createNewPost = async (authorId: string, content: string, imageUrl?: string, videoUrl?: string): Promise<IPost> => {
@@ -11,3 +11,26 @@ export const createNewPost = async (authorId: string, content: string, imageUrl?
     return createPost({ author: authorObjectId, content, imageUrl, videoUrl });
   };
 
+  export const getFeedPosts = async (userId: string, page: number, limit: number): Promise<IPost[]> => {
+    try {
+      const user = await findUserById(userId);
+      if (!user) {
+        throw new HttpError(404, 'User not found');
+      }
+  
+      // Convert user.following from ObjectId[] to string[]
+      const followingIds = user.following.map(item => item.toString());
+  
+      // Convert followingIds to ObjectId array
+      const objectIdArray = followingIds.map(id => new mongoose.Types.ObjectId(id));
+  
+      // Fetch posts by authorIds (followingIds) with pagination
+      const posts = await findPostsByAuthorIds(objectIdArray, page, limit);
+  
+      return posts;
+    } catch (error) {
+      console.error('Error fetching feed posts:', error);
+      throw new HttpError(409, 'Failed to fetch feed posts');
+    }
+  
+  };
